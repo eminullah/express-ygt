65cb753ce39d7c364c801cc07a917928c2f9028b
eminullah <143505176+eminullah@users.noreply.github.com>
2023-08-29 19:43:21 +0300

    img değiştrildi

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ const html = `
     <!-- Carousel Start -->
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
-            <div class="owl-carousel-item position-relative" data-dot="<img src='imgcarousel-1.jpg'>">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
                 <img class="img-fluid" src="img/carousel-1.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
@@ -135,7 +135,7 @@ const html = `
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='imgcarousel-2.jpg'>">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-2.jpg'>">
                 <img class="img-fluid" src="img/carousel-2.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
@@ -149,7 +149,7 @@ const html = `
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='imgcarousel-3.jpg'>">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-3.jpg'>">
                 <img class="img-fluid" src="img/carousel-3.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
