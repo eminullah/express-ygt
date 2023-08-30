e556070f8e2b466ef2ea7c3e961088f1f751a183
eminullah <143505176+eminullah@users.noreply.github.com>
2023-08-30 13:58:41 +0300

    yeni resimler eklendi.

--- app.js
+++ app.js
@@ -121,43 +121,43 @@ const html = `
     <!-- Carousel Start -->
     <div class="container-fluid p-0 pb-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="owl-carousel header-carousel position-relative">
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
-                <img class="img-fluid" src="img/carousel-1.jpg" alt="">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='img/c1.jpg'>">
+                <img class="img-fluid" src="img/c1.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-10 col-lg-8">
-                                <h1 class="display-2 text-white animated slideInDown">Doğal Gaz Isı Dönüşümleri</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                                <h1 class="display-2 text-white animated slideInDown">Doğalgaz Isı Dönüşümleri</h1>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Okullar, İş yerleri, Hastaneler,  Fabrikalar için doğalgaz ısı dönüşüm sistemlerinin kurulumu, bakım-onarımı</p>
+                                <!--<a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>-->
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-2.jpg'>">
-                <img class="img-fluid" src="img/carousel-2.jpg" alt="">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='img/c2.jpg'>">
+                <img class="img-fluid" src="img/c2.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-10 col-lg-8">
                                 <h1 class="display-2 text-white animated slideInDown">Otomasyon</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Doğalgaz ısı dönüşüm projelerinde anahtar teslim sistemler</p>
+                                <!--<a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>-->
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-3.jpg'>">
-                <img class="img-fluid" src="img/carousel-3.jpg" alt="">
+            <div class="owl-carousel-item position-relative" data-dot="<img src='img/c3.jpg'>">
+                <img class="img-fluid" src="img/c3.jpg" alt="">
                 <div class="owl-carousel-inner">
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-10 col-lg-8">
                                 <h1 class="display-2 text-white animated slideInDown">Mühendislik</h1>
-                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p>
-                                <a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>
+                                <p class="fs-5 fw-medium text-white mb-4 pb-3">Doğalgaz ısı dönüşüm sistemleri projelendirme, kurulum ve teknik destek</p>
+                                <!--<a href="" class="btn btn-primary rounded-pill py-3 px-5 animated slideInLeft">Read More</a>-->
                             </div>
                         </div>
                     </div>
